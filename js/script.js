cef22a9a69a1386ea7a7524372d4977420043f50
Noxxer <96419919+PargonX@users.noreply.github.com>
2024-02-18 14:51:15 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var url = "https://api.minetools.eu/ping/play.vonix.network/25738";
+// example = "https://api.minetools.eu/ping/youtserver.com/12345"
+
+$.getJSON(url, function(r) {
+    //data is the JSON string
+ if(r.error){
+    $('#rest').html('Server Offline');
+   return false;
+ } 
+var pl = '';
+ if(r.players.sample.length > 0 ){
+   //pl = '<br>Último online: '+r.players.sample[0].name;
+   pl = '';
+ } 
+  $('#rest').html('<i class="fas fa-users"></i> <span class="azul">Players Online:</span> <b><span class="online_players">'+r.players.online+'</span>/<span class="rojo">'+r.players.max+pl + "</span></b>");
+ //$('#favicon').attr('src', r.favicon);
+    
+});
